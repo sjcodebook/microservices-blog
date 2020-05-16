187e4ff58399e4d8919b6c0af121df6547566ff2
Sanmati Kumar Jain <i.sanmati.jain@redcarpetup.com>
2020-05-16 23:53:20 +0530

    implementing query service

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const { randomBytes } = require('crypto')
 const cors = require('cors')
 const axios = require('axios')
 
@@ -7,23 +6,16 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-// const posts = {}
-
-// app.get('/posts', (req, res) => {
-//   res.send(posts)
-// })
-
 app.post('/events', (req, res) => {
-  const id = randomBytes(4).toString('hex')
   const event = req.body
 
   console.log(event)
 
   axios.post('http://localhost:4000/events', event)
   axios.post('http://localhost:4001/events', event)
-  // axios.post('http://localhost:4002', event)
+  axios.post('http://localhost:4002/events', event)
 
-  res.status(200).send(id)
+  res.status(200).send(event)
 })
 
 app.listen(4005, () => {
