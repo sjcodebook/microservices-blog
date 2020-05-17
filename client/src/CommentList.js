2139b3afb026e2022ac0081bbd4294eedba0a0f6
Sanmati Kumar Jain <i.sanmati.jain@redcarpetup.com>
2020-05-17 20:34:16 +0530

    implement comment moderation on client side

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React from 'react'
 
 export default ({ comments }) => {
+  const renderedComments = comments.map((comment) => {
+    if (comment.status === 'Pending') {
+      comment.content = 'This comment is awaiting moderation'
+    }
+    if (comment.status === 'Rejected') {
+      comment.content = 'This comment is rejected'
+    }
+    return <li key={comment.id}>{comment.content}</li>
+  })
 
-  const renderedComments = comments.map(comment => {
-    return <li key={comment.id}>{comment.content}</li>;
-  });
-
-  return <ul>{renderedComments}</ul>;
-};
+  return <ul>{renderedComments}</ul>
+}
--- comments/index.js
+++ comments/index.js
@@ -28,7 +28,7 @@ app.post('/posts/:id/comments', async (req, res) => {
       id: commentId,
       content,
       postId: req.params.id,
-      status: 'pending',
+      status: 'Pending',
     },
   })
 
